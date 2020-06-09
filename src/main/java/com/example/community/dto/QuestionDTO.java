305d985684bd0deee6c01ec0391d7253fc9743b2
ahlin <844123045@qq.com>
2020-06-09 20:21:35 +0800

    add questionDTO

---
+++ src/main/java/com/example/community/dto/QuestionDTO.java
@@ -0,0 +1,19 @@
+package com.example.community.dto;
+
+import com.example.community.model.User;
+import lombok.Data;
+
+@Data
+public class QuestionDTO {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer creator;
+    private Integer viewCount;
+    private Integer likeCount;
+    private Integer commentCount;
+    private User user;
+}
